1d11b9da479d742ed02eef195f44b714ffef19a7
Momchil <leatherpuffy@gmail.com>
2020-01-20 12:24:46 +0200

    clean up

--- 05-C#-Web-Basics/Exam/Apps/SULS/SULS.Data/DatabaseConfiguration.cs
+++ 05-C#-Web-Basics/Exam/Apps/SULS/SULS.Data/DatabaseConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseConfiguration
     {
         public const string ConnectionString =
-            @"Server=.;Database=SulsDb-violeta_kirova;Trusted_Connection=True;Integrated Security=True;";
+            @"connectionString";
     }
 }
